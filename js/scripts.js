83ece24f700798d3f9bf9f7b1acd5cbf1015d27c
Derek Villars and Shane Villars <shane@>
2017-01-17 13:40:44 -0800

    add refactor using for loop instead of forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//
+//     var vowels = ['a','e','i','o','u'];
+//     var data = $(".original").val();
+//     $(".original").val('');
+//     var elements = data.split('');
+//     var coded = [];
+//
+//     elements.forEach(function(element) {
+//       if (vowels.indexOf(element) === -1) {
+//         coded.push(element);
+//       } else {
+//         coded.push('-');
+//       }
+//     });
+//     var final = '';
+//     coded.forEach(function(element) {
+//       final += element;
+//     });
+//     $(".coded").text(final);
+//   });
+// });
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
@@ -8,17 +33,17 @@ $(document).ready(function() {
     var elements = data.split('');
     var coded = [];
 
-    elements.forEach(function(element) {
-      if (vowels.indexOf(element) === -1) {
-        coded.push(element);
+    for (var index = 0; index < elements.length; index += 1) {
+      if (vowels.indexOf(elements[index]) === -1) {
+        coded.push(elements[index]);
       } else {
         coded.push('-');
       }
-    });
+    };
     var final = '';
-    coded.forEach(function(element) {
-      final += element;
-    });
+    for (var index = 0; index < coded.length; index += 1) {
+      final += coded[index];
+    };
     $(".coded").text(final);
   });
 });
