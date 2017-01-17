d3e709c5bde9cc9a14145d3bfd272654decf3150
Derek Villars and Shane Villars <shane@>
2017-01-17 10:20:16 -0800

    add clear textbox value after submit

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,9 @@ $(document).ready(function() {
 
     var vowels = ['a','e','i','o','u'];
     var data = $(".original").val();
+    $(".original").val('');
     var elements = data.split('');
     var coded = [];
-
-
 
     elements.forEach(function(element) {
       if (vowels.indexOf(element) === -1) {
